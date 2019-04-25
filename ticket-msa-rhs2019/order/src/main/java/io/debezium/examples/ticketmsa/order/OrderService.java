43d79774abdad02665b82385386157482354ea78
Jiri Pechanec <jpechane@redhat.com>
2019-04-25 14:58:48 +0200

    use emitter to cross imperative/reactive

--- ticket-msa-rhs2019/order/src/main/java/io/debezium/examples/ticketmsa/order/OrderService.java
+++ ticket-msa-rhs2019/order/src/main/java/io/debezium/examples/ticketmsa/order/OrderService.java
@@ -1,9 +1,7 @@
 package io.debezium.examples.ticketmsa.order;
 
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
-
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
@@ -12,29 +10,21 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
-import org.eclipse.microprofile.reactive.messaging.Outgoing;
-
 import io.debezium.examples.ticketmsa.order.model.Order;
+import io.smallrye.reactive.messaging.annotations.Emitter;
+import io.smallrye.reactive.messaging.annotations.Stream;
 import io.smallrye.reactive.messaging.kafka.KafkaMessage;
 
 @Path("/orders")
 @ApplicationScoped
 public class OrderService {
 
+    @Inject
+    @Stream("orders")
+    private Emitter<KafkaMessage<Integer, String>> orderStream;
+
     @PersistenceContext
     private EntityManager entityManager;
-    private final BlockingQueue<Order> queue = new ArrayBlockingQueue<>(1);
-
-    @Outgoing("orders") 
-    public KafkaMessage<Integer, String> deliver() {
-        try {
-            final Order order = queue.take();
-            return KafkaMessage.of(order.getId(), order.toJson().toString());
-        }
-        catch (InterruptedException e) {
-            throw new IllegalStateException(e);
-        }
-    }
 
     @POST
     @Transactional
@@ -42,12 +32,7 @@ public class OrderService {
     @Consumes("application/json")
     public Order addOrder(Order order) {
         order = entityManager.merge(order);
-        try {
-            queue.put(order);
-        }
-        catch (InterruptedException e) {
-            throw new IllegalStateException(e);
-        }
-       return order;
+        orderStream.send(KafkaMessage.of(order.getId(), order.toJson().toString()));
+        return order;
     }
 }
