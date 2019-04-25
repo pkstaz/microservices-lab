76ce976fcfe33b9b757df0c1936403d95dd8790f
Gunnar Morling <gunnar.morling@googlemail.com>
2019-04-25 15:21:09 +0200

    ticket-msa-rhs2019 -> ticket-msa

--- ticket-msa-rhs2019/order/src/main/java/io/debezium/examples/ticketmsa/order/RestApplication.java
+++
@@ -1,8 +0,0 @@
-package io.debezium.examples.ticketmsa.order;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/rest")
-public class RestApplication extends Application {
-}
--- ticket-msa/order/src/main/java/io/debezium/examples/ticketmsa/order/OrderService.java
+++ ticket-msa/order/src/main/java/io/debezium/examples/ticketmsa/order/OrderService.java
@@ -2,7 +2,6 @@ package io.debezium.examples.ticketmsa.order;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import javax.json.JsonObject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
@@ -11,27 +10,21 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
-import org.aerogear.kafka.SimpleKafkaProducer;
-import org.aerogear.kafka.cdi.annotation.KafkaConfig;
-import org.aerogear.kafka.cdi.annotation.Producer;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
-
 import io.debezium.examples.ticketmsa.order.model.Order;
+import io.smallrye.reactive.messaging.annotations.Emitter;
+import io.smallrye.reactive.messaging.annotations.Stream;
+import io.smallrye.reactive.messaging.kafka.KafkaMessage;
 
 @Path("/orders")
 @ApplicationScoped
-@KafkaConfig(bootstrapServers = "#{KAFKA_SERVICE_HOST}:#{KAFKA_SERVICE_PORT}")
 public class OrderService {
 
     @Inject
-    @ConfigProperty(name="order.topic.name", defaultValue="orders")
-    private String topicName;
+    @Stream("orders")
+    private Emitter<KafkaMessage<Integer, String>> orderStream;
 
     @PersistenceContext
     private EntityManager entityManager;
-
-    @Producer
-    private SimpleKafkaProducer<Integer, JsonObject> kafka;
 
     @POST
     @Transactional
@@ -39,7 +32,7 @@ public class OrderService {
     @Consumes("application/json")
     public Order addOrder(Order order) {
         order = entityManager.merge(order);
-        kafka.send(topicName, order.getId(), order.toJson());
+        orderStream.send(KafkaMessage.of(order.getId(), order.toJson().toString()));
         return order;
     }
 }
